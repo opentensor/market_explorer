d975cfb1395cc7994ef8dd7e36ebc444aac6e581
unconst <jake@bittensor.com>
2021-11-28 16:18:48 -0500

    ss

--- pull.py
+++ pull.py
@@ -17,15 +17,17 @@ def pull_graph_at_block( block ):
             graph = bittensor.metagraph( subtensor = sub )
             graph.sync( block )
             # 99% pulled
-            if sum( [ 1 for hotkey in graph.hotkeys if hotkey != '' ]) / graph.n.item() > 0.99:
+            n_pulled = float(sum( [ 1 for hotkey in graph.hotkeys if hotkey != '' ]))
+            frac = n_pulled / graph.n.item() if graph.n.item() != 0 else 1
+            if frac > 0.99:
                 graph.save_to_path( path = os.path.expanduser('~/data/'), filename = 'nakamoto-{}'.format( block ) )
                 print ('finished pulling block: {}'.format( block ))
                 break
             else:
-                print ('restarting pulling block: {}'.format( block ))
+                print ('restarting pulling block: {} failed from frac: {} '.format( block, frac ))
                 continue
-        except:
-            print ('restarting pulling block: {}'.format( block ))
+        except Exception as e:
+            print ('restarting pulling block: {} with error: {}'.format( block, e ))
             continue
 
 def multithread_pull_range( block_range ):
@@ -58,7 +60,7 @@ if __name__ == "__main__":
     all_ranges = []
     block_range_size = 5
     current_range = []
-    for block in range( 0, sub.get_current_block(), step_size ):
+    for block in range( 1000, sub.get_current_block(), step_size ):
         if block not in already_pulled_blocks:
             if len(current_range) == block_range_size:
                 all_ranges.append( current_range )

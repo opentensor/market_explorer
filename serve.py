566fedf23bf685ce56ff12e122570ec288e7ba05
unconst <jake@bittensor.com>
2021-11-28 22:31:42 -0500

    s

--- serve.py
+++ serve.py
@@ -36,7 +36,13 @@ app.layout = html.Div(
     className = "page",
     style = {'backgroundColor': '#111111' },
     children = [
-        dcc.Graph( id='block_to_n', figure = n_neurons_fig),
+        dcc.Dropdown(
+            id = 'uid_dropdown',
+            options = [ {'label':str(int(u)), 'value': int(u)} for u in list(df[df.index.max()].uid) ],
+            value = 0,
+            style = {'backgroundColor': '#111111' }
+        ),
+        dcc.Graph(id='incentive_over_time'),
         dcc.Graph( id='uid_to_incentive' ),
         dcc.Slider(
             id='uid_to_incentive_slider',
@@ -45,14 +51,7 @@ app.layout = html.Div(
             value = df.index.min(),
             step = 1000
         ),
-
-        dcc.Dropdown(
-            id = 'uid_dropdown',
-            options = [ {'label':str(int(u)), 'value': int(u)} for u in list(df[df.index.max()].uid) ],
-            value = 0,
-            style = {'backgroundColor': '#111111' }
-        ),
-        dcc.Graph(id='incentive_over_time'),
+        dcc.Graph( id='block_to_n', figure = n_neurons_fig),
     ]
 )
 

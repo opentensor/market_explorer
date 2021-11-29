063d5d259ad532b38dc51c27196d36cecd165ec5
unconst <jake@bittensor.com>
2021-11-29 08:06:41 -0500

    done

--- serve.py
+++ serve.py
@@ -36,6 +36,14 @@ app.layout = html.Div(
     className = "page",
     style = {'backgroundColor': '#111111' },
     children = [
+        html.Div(
+            html.Img(
+                src=app.get_asset_url(
+                    "assets/tau_inverted.png"
+                ),
+                className="tau",
+            )
+        ),
         dcc.Dropdown(
             id = 'uid_dropdown',
             options = [ {'label':str(int(u)), 'value': int(u)} for u in list(df[df.index.max()].uid) ],

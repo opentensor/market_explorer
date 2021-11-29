68e5eec894399ebaa97a860b9f2812104f526587
unconst <jake@bittensor.com>
2021-11-29 08:48:20 -0500

    s

--- serve.py
+++ serve.py
@@ -5,6 +5,7 @@ from dash import html
 from dash.dependencies import Input, Output
 import plotly.express as px
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 import pandas as pd
 
@@ -44,14 +45,15 @@ app.layout = html.Div(
             value = 0,
             style = {'backgroundColor': '#000000' }
         ),
-        dcc.Graph( id='stake_over_time' ),
-        dcc.Graph( id='rank_over_time' ),
-        dcc.Graph( id='trust_over_time' ),
-        dcc.Graph( id='consensus_over_time' ),
-        dcc.Graph( id='incentive_over_time' ),
-        dcc.Graph( id='dividends_over_time' ),
-        dcc.Graph( id='emission_over_time' ),
-        dcc.Graph( id='uid_to_incentive' ),
+        dcc.Graph( id='values_over_time' ),
+        # dcc.Graph( id='stake_over_time' ),
+        # dcc.Graph( id='rank_over_time' ),
+        # dcc.Graph( id='trust_over_time' ),
+        # dcc.Graph( id='consensus_over_time' ),
+        # dcc.Graph( id='incentive_over_time' ),
+        # dcc.Graph( id='dividends_over_time' ),
+        # dcc.Graph( id='emission_over_time' ),
+        # dcc.Graph( id='uid_to_incentive' ),
         dcc.Slider(
             id='uid_to_incentive_slider',
             min = df.index.min(),
@@ -80,15 +82,7 @@ def update_uid_to_incentive ( selected_block ):
     return fig
 
 @app.callback(
-    [ 
-        Output('stake_over_time', 'figure'),
-        Output('rank_over_time', 'figure'), 
-        Output('trust_over_time', 'figure'), 
-        Output('consensus_over_time', 'figure'), 
-        Output('incentive_over_time', 'figure'), 
-        Output('dividends_over_time', 'figure'), 
-        Output('emission_over_time', 'figure'), 
-    ],
+    Output('values_over_time', 'figure'),
     Input('uid_dropdown', 'value')
 )
 def update_incentive_over_time ( selected_uid ):
@@ -98,7 +92,7 @@ def update_incentive_over_time ( selected_uid ):
     trust = [ block['trust'][selected_uid] for block in df ]
     rank = [ block['rank'][selected_uid] for block in df ]
     dividends = [ block['dividends'][selected_uid] for block in df ]
-    emission = [ block['emission'][selected_uid] for block in df ]
+    # emission = [ block['emission'][selected_uid] for block in df ]
     consensus = [ block['consensus'][selected_uid] for block in df ]
 
     # Get sorted data.
@@ -109,18 +103,65 @@ def update_incentive_over_time ( selected_uid ):
     yy_conensus = [y for _, y in sorted(zip(x, consensus))]
     yy_incentive = [y for _, y in sorted(zip(x, incentive))]
     yy_dividends = [y for _, y in sorted(zip(x, dividends))]
-    yy_emission = [y for _, y in sorted(zip(x, emission))]
+    # yy_emission = [y for _, y in sorted(zip(x, emission))]
+
+    fig = make_subplots(rows=2, cols=3)
+    # fig.add_trace( px.line( x=xx, y=yy_stake, template='plotly_dark', markers=True, title="Stake @ uid: {}".format( selected_uid ), labels=dict(x="block", y="stake") ), row=1, col=1 )
+    # fig.update_layout(height=600, width=800, title_text="Side By Side Subplots")
 
     # Build scatters.
-    stake_over_time = px.line( x=xx, y=yy_stake, template='plotly_dark', markers=True, title="Stake @ uid: {}".format( selected_uid ), labels=dict(x="block", y="stake") )
-    rank_over_time = px.line( x=xx, y=yy_rank, template='plotly_dark', markers=True, title="Rank @ uid: {}".format( selected_uid ), labels=dict(x="block", y="rank") )
-    trust_over_time = px.line( x=xx, y=yy_trust, template='plotly_dark', markers=True, title="Trust @ uid: {}".format( selected_uid ), labels=dict(x="block", y="trust") )
-    consensus_over_time = px.line( x=xx, y=yy_conensus, template='plotly_dark', markers=True, title="Consensus @ uid: {}".format( selected_uid ), labels=dict(x="block", y="consensus") )
-    incentive_over_time = px.line( x=xx, y=yy_incentive, template='plotly_dark', markers=True, title="Incentive @ uid: {}".format( selected_uid ), labels=dict(x="block", y="incentive") )
-    dividends_over_time = px.line( x=xx, y=yy_dividends, template='plotly_dark', markers=True, title="Dividends @ uid: {}".format( selected_uid ), labels=dict(x="block", y="dividends") )
-    emission_over_time = px.line( x=xx, y=yy_emission, template='plotly_dark', markers=True, title="Emission @ uid: {}".format( selected_uid ), labels=dict(x="block", y="emission") )
+    fig.add_trace( px.line( x=xx, y=yy_stake, template='plotly_dark', markers=True, title="Stake @ uid: {}".format( selected_uid ), labels=dict(x="block", y="stake") ), row=1, col=1 )
+    fig.add_trace( px.line( x=xx, y=yy_rank, template='plotly_dark', markers=True, title="Rank @ uid: {}".format( selected_uid ), labels=dict(x="block", y="rank") ), row=1, col=2 )
+    fig.add_trace( px.line( x=xx, y=yy_trust, template='plotly_dark', markers=True, title="Trust @ uid: {}".format( selected_uid ), labels=dict(x="block", y="trust") ), row=1, col=3 )
+    fig.add_trace( px.line( x=xx, y=yy_conensus, template='plotly_dark', markers=True, title="Consensus @ uid: {}".format( selected_uid ), labels=dict(x="block", y="consensus") ), row=2, col=1 )
+    fig.add_trace( px.line( x=xx, y=yy_incentive, template='plotly_dark', markers=True, title="Incentive @ uid: {}".format( selected_uid ), labels=dict(x="block", y="incentive") ), row=2, col=2 )
+    fig.add_trace( px.line( x=xx, y=yy_dividends, template='plotly_dark', markers=True, title="Dividends @ uid: {}".format( selected_uid ), labels=dict(x="block", y="dividends") ), row=2, col=3 )
+    # fig.add_trace( px.line( x=xx, y=yy_emission, template='plotly_dark', markers=True, title="Emission @ uid: {}".format( selected_uid ), labels=dict(x="block", y="emission") ), row=1, col=1 )
 
-    return stake_over_time, rank_over_time, trust_over_time, consensus_over_time, incentive_over_time, dividends_over_time, emission_over_time
+    return fig
+
+# @app.callback(
+#     [ 
+#         Output('stake_over_time', 'figure'),
+#         Output('rank_over_time', 'figure'), 
+#         Output('trust_over_time', 'figure'), 
+#         Output('consensus_over_time', 'figure'), 
+#         Output('incentive_over_time', 'figure'), 
+#         Output('dividends_over_time', 'figure'), 
+#         Output('emission_over_time', 'figure'), 
+#     ],
+#     Input('uid_dropdown', 'value')
+# )
+# def update_incentive_over_time ( selected_uid ):
+#     x = list(df.index)
+#     incentive = [ block['incentive'][selected_uid] for block in df ]
+#     stake = [ block['stake'][selected_uid] for block in df ]
+#     trust = [ block['trust'][selected_uid] for block in df ]
+#     rank = [ block['rank'][selected_uid] for block in df ]
+#     dividends = [ block['dividends'][selected_uid] for block in df ]
+#     emission = [ block['emission'][selected_uid] for block in df ]
+#     consensus = [ block['consensus'][selected_uid] for block in df ]
+
+#     # Get sorted data.
+#     xx = [x for x, _ in sorted(zip(x, incentive))]
+#     yy_stake = [y for _, y in sorted(zip(x, stake))]
+#     yy_rank = [y for _, y in sorted(zip(x, rank))]
+#     yy_trust = [y for _, y in sorted(zip(x, trust))]
+#     yy_conensus = [y for _, y in sorted(zip(x, consensus))]
+#     yy_incentive = [y for _, y in sorted(zip(x, incentive))]
+#     yy_dividends = [y for _, y in sorted(zip(x, dividends))]
+#     yy_emission = [y for _, y in sorted(zip(x, emission))]
+
+#     # Build scatters.
+#     stake_over_time = px.line( x=xx, y=yy_stake, template='plotly_dark', markers=True, title="Stake @ uid: {}".format( selected_uid ), labels=dict(x="block", y="stake") )
+#     rank_over_time = px.line( x=xx, y=yy_rank, template='plotly_dark', markers=True, title="Rank @ uid: {}".format( selected_uid ), labels=dict(x="block", y="rank") )
+#     trust_over_time = px.line( x=xx, y=yy_trust, template='plotly_dark', markers=True, title="Trust @ uid: {}".format( selected_uid ), labels=dict(x="block", y="trust") )
+#     consensus_over_time = px.line( x=xx, y=yy_conensus, template='plotly_dark', markers=True, title="Consensus @ uid: {}".format( selected_uid ), labels=dict(x="block", y="consensus") )
+#     incentive_over_time = px.line( x=xx, y=yy_incentive, template='plotly_dark', markers=True, title="Incentive @ uid: {}".format( selected_uid ), labels=dict(x="block", y="incentive") )
+#     dividends_over_time = px.line( x=xx, y=yy_dividends, template='plotly_dark', markers=True, title="Dividends @ uid: {}".format( selected_uid ), labels=dict(x="block", y="dividends") )
+#     emission_over_time = px.line( x=xx, y=yy_emission, template='plotly_dark', markers=True, title="Emission @ uid: {}".format( selected_uid ), labels=dict(x="block", y="emission") )
+
+#     return stake_over_time, rank_over_time, trust_over_time, consensus_over_time, incentive_over_time, dividends_over_time, emission_over_time
 
 if __name__ == '__main__':
     app.run_server(host = '0.0.0.0', debug=True)

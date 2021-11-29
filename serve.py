c05367cee150cbb49ac8fbf0cb8dd4480366e628
unconst <jake@bittensor.com>
2021-11-28 22:10:52 -0500

    s

--- serve.py
+++ serve.py
@@ -16,7 +16,16 @@ blocks = list(df.index)
 n_neurons = [ len(d) for d in df ]
 
 n_neurons_fig = go.Figure()
-n_neurons_fig.add_trace( go.Scatter( x = blocks, y = n_neurons, template='plotly_dark' ))
+n_neurons_fig.add_trace( 
+    go.Scatter ( 
+        x = blocks, 
+        y = n_neurons, 
+        template='plotly_dark', 
+        markers=True, 
+        labels=dict(x="block", y="n"),
+        title="Neurons",
+    )
+)
 n_neurons_fig.update_layout(title_text="N/block")
 n_neurons_fig.update_layout(
     xaxis=dict (

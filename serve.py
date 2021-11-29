08ac0dac4906abfd03de449f7128cbd078626a95
unconst <jake@bittensor.com>
2021-11-29 08:40:33 -0500

    s

--- serve.py
+++ serve.py
@@ -44,9 +44,13 @@ app.layout = html.Div(
             value = 0,
             style = {'backgroundColor': '#000000' }
         ),
-        dcc.Graph( id='incentive_over_time' ),
         dcc.Graph( id='stake_over_time' ),
-
+        dcc.Graph( id='rank_over_time' ),
+        dcc.Graph( id='trust_over_time' ),
+        dcc.Graph( id='consensus_over_time' ),
+        dcc.Graph( id='incentive_over_time' ),
+        dcc.Graph( id='dividends_over_time' ),
+        dcc.Graph( id='emission_over_time' ),
         dcc.Graph( id='uid_to_incentive' ),
         dcc.Slider(
             id='uid_to_incentive_slider',
@@ -83,16 +87,32 @@ def update_incentive_over_time ( selected_uid ):
     x = list(df.index)
     incentive = [ block['incentive'][selected_uid] for block in df ]
     stake = [ block['stake'][selected_uid] for block in df ]
+    trust = [ block['trust'][selected_uid] for block in df ]
+    rank = [ block['rank'][selected_uid] for block in df ]
+    dividends = [ block['dividends'][selected_uid] for block in df ]
+    emission = [ block['emission'][selected_uid] for block in df ]
+    consensus = [ block['consensus'][selected_uid] for block in df ]
 
     # Get sorted data.
     xx = [x for x, _ in sorted(zip(x, incentive))]
-    yy_incentive = [y for _, y in sorted(zip(x, incentive))]
     yy_stake = [y for _, y in sorted(zip(x, stake))]
+    yy_rank = [y for _, y in sorted(zip(x, stake))]
+    yy_trust = [y for _, y in sorted(zip(x, trust))]
+    yy_conensus = [y for _, y in sorted(zip(x, consensus))]
+    yy_incentive = [y for _, y in sorted(zip(x, incentive))]
+    yy_dividends = [y for _, y in sorted(zip(x, dividends))]
+    yy_emission = [y for _, y in sorted(zip(x, emission))]
 
     # Build scatters.
-    incentive_over_time = px.line( x=xx, y=yy_incentive, template='plotly_dark', markers=True, title="Incentive @ uid: {}".format( selected_uid ), labels=dict(x="block", y="incentive") )
     stake_over_time = px.line( x=xx, y=yy_stake, template='plotly_dark', markers=True, title="Stake @ uid: {}".format( selected_uid ), labels=dict(x="block", y="stake") )
-    return incentive_over_time, stake_over_time
+    rank_over_time = px.line( x=xx, y=yy_stake, template='plotly_dark', markers=True, title="Rank @ uid: {}".format( selected_uid ), labels=dict(x="block", y="rank") )
+    trust_over_time = px.line( x=xx, y=yy_stake, template='plotly_dark', markers=True, title="Trust @ uid: {}".format( selected_uid ), labels=dict(x="block", y="trust") )
+    consensus_over_time = px.line( x=xx, y=yy_stake, template='plotly_dark', markers=True, title="Consensus @ uid: {}".format( selected_uid ), labels=dict(x="block", y="consensus") )
+    incentive_over_time = px.line( x=xx, y=yy_incentive, template='plotly_dark', markers=True, title="Incentive @ uid: {}".format( selected_uid ), labels=dict(x="block", y="incentive") )
+    dividends_over_time = px.line( x=xx, y=yy_stake, template='plotly_dark', markers=True, title="Dividends @ uid: {}".format( selected_uid ), labels=dict(x="block", y="dividends") )
+    emission_over_time = px.line( x=xx, y=yy_stake, template='plotly_dark', markers=True, title="Emission @ uid: {}".format( selected_uid ), labels=dict(x="block", y="emission") )
+
+    return stake_over_time, rank_over_time, trust_over_time, consensus_over_time, incentive_over_time, dividends_over_time, emission_over_time
 
 if __name__ == '__main__':
     app.run_server(host = '0.0.0.0', debug=True)

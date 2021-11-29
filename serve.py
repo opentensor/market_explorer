dd065d5062bf380493997776293c83c8713ec088
unconst <jake@bittensor.com>
2021-11-28 22:07:30 -0500

    ss

--- serve.py
+++ serve.py
@@ -1,100 +1,83 @@
 import dash
-import bittensor
+import os
 from dash import dcc
 from dash import html
+from dash.dependencies import Input, Output
 import plotly.express as px
-import pandas as pd
 import plotly.graph_objects as go
-import plotly.figure_factory as ff
-import os
 
+import pandas as pd
+
+df = pd.read_pickle( os.path.expanduser('~/data/all_blocks.pd') )
+df = df.sort_index()
 app = dash.Dash(__name__)
 
-def create_layout():
+blocks = list(df.index)
+n_neurons = [ len(d) for d in df ]
 
-    graph = bittensor.metagraph().load_from_path( os.path.expanduser('~/nakamoto-latest') )
+n_neurons_fig = go.Figure()
+n_neurons_fig.add_trace( go.Scatter( x = blocks, y = n_neurons ))
+n_neurons_fig.update_layout(title_text="N/block")
+n_neurons_fig.update_layout(
+    xaxis=dict (
+        rangeslider=dict(
+            visible=True
+        ),
+        type="block"
+    )
+)
 
-    query_df = pd.read_json(os.path.expanduser('~/query-latest.json') ).T
-    df = pd.DataFrame(columns=['uid', 'active', 'stake','rank','trust', 'consensus', 'incentive', 'dividends', 'emission', 'code', 'time', 'success'], index=graph.uids.tolist())
-    for uid in graph.uids.tolist():
-        try:
-            c = query_df.code[uid]
-            t = query_df.time[uid]
-            s = query_df.code[uid] == 1
-        except:
-            c = 23
-            t = 12.0
-            s = False
-        df.loc[uid] = pd.Series({
-        'uid':uid, 
-        'active': graph.active[uid].item(), 
-        'stake':graph.S[uid].item(), 
-        'rank':graph.R[uid].item(), 
-        'trust':graph.T[uid].item(), 
-        'consensus':graph.C[uid].item(), 
-        'incentive':graph.I[uid].item(), 
-        'dividends':graph.D[uid].item(), 
-        'emission':graph.E[uid].item(),
-        'code':c,
-        'time': t,
-        'success': s,
-    })
+app.layout = html.Div(
+    className = "page",
+    style = {'backgroundColor': 'black'},
+    children = [
+        dcc.Graph( id='block_to_n', figure = n_neurons_fig),
+        dcc.Graph( id='uid_to_incentive' ),
+        dcc.Slider(
+            id='uid_to_incentive_slider',
+            min = df.index.min(),
+            max = df.index.max(),
+            value = df.index.min(),
+            step = 1000
+        ),
 
-    query_suc = df.loc[df['success'] == True]
-    query_fig = px.scatter(query_suc, x="uid", y="time", color="success")
-    query_hist = px.histogram(query_suc, x="time", color="success")
-    query_to_incentive = px.scatter(query_suc, x="time", y="incentive")
+        dcc.Dropdown(
+            id = 'uid_dropdown',
+            options = [ {'label':str(int(u)), 'value': int(u)} for u in list(df[df.index.max()].uid) ],
+            value = 0,
+            style = {'backgroundColor': 'black'}
+        ),
+        dcc.Graph(id='incentive_over_time'),
+    ]
+)
 
-    fig7 = go.Figure(data=[go.Table(
-        header=dict(values=list(df.columns),
-                    fill_color='paleturquoise',
-                    align='left'),
-        cells=dict(values=df.transpose().values.tolist(),
-                fill_color='lavender',
-                align='left'))
-    ])
+@app.callback(
+    Output('uid_to_incentive', 'figure'),
+    Input('uid_to_incentive_slider', 'value')
+)
+def update_uid_to_incentive ( selected_block ):
+    closest_index = list(df.index)[0]
+    for val in list(df.index):
+        if val <= selected_block:
+            closest_index = val
+        else:
+            break
+    filtered_df = df[ closest_index ]
+    fig = px.scatter( filtered_df, x="uid", y="incentive", template='plotly_dark', marginal_y="histogram", title="Incentive @ block:{}".format( selected_block ), labels=dict(x="uid", y="incentive") )
+    fig.update_layout( transition_duration=500 )
+    return fig
 
-    fig1 = px.scatter(df, x="uid", y="stake", color="active")
-    fig11 = px.histogram(df, x="stake", color="active")
-    fig2 = px.scatter(df, x="uid", y="rank", color="active" )
-    fig3 = px.scatter(df, x="uid", y="trust", color="active" )
-    fig4 = px.scatter(df, x="uid", y="consensus", color="active" )
-    fig5 = px.scatter(df, x="uid", y="incentive", color="active" )
-    fig6 = px.scatter(df, x="uid", y="dividends", color="active" )
-
-    tvc = px.scatter(df, x="trust", y="consensus", marginal_x="histogram", marginal_y="rug")
-    ivc = px.scatter(df, x="incentive", y="consensus", marginal_x="histogram", marginal_y="rug")
-
-    WT = go.Figure(data=go.Heatmap( z=(graph.W > 0).int().tolist() ))
-    BT = go.Figure(data=go.Heatmap( z=(graph.B > 0).int().tolist() ))
-
-    WT.update_layout(autosize=False, width=2000,  height=2000)
-    BT.update_layout(autosize=False, width=2000,  height=2000)
-
-    markdown_text = '''
-    ### Nakamoto network explorer
-    '''
-    return html.Div(children=[
-        html.H1(children='Bittensor'),
-        dcc.Markdown(children=markdown_text),
-        dcc.Graph(id='query_fig', figure=query_fig),
-        dcc.Graph(id='query_hist', figure=query_hist),
-        dcc.Graph(id='query time to incentive', figure=query_to_incentive),
-        dcc.Graph(id='stake', figure=fig1),
-        dcc.Graph(id='stake_hist', figure=fig11),
-        dcc.Graph(id='ranks', figure=fig2),
-        dcc.Graph(id='trust', figure=fig3),
-        dcc.Graph(id='consensus', figure=fig4),
-        dcc.Graph(id='trust_by_consensus', figure=tvc),
-        dcc.Graph(id='incentive_by_consensus', figure=ivc),
-        dcc.Graph(id='incentive', figure=fig5),
-        dcc.Graph(id='dividends', figure=fig6),
-        dcc.Graph(id='weights', figure=WT),
-        dcc.Graph(id='bonds', figure=BT),
-        dcc.Graph(id='metagraph', figure=fig7),
-    ])
-
-app.layout = create_layout
+@app.callback(
+    Output('incentive_over_time', 'figure'),
+    Input('uid_dropdown', 'value')
+)
+def update_incentive_over_time ( selected_uid ):
+    x = list(df.index)
+    y = [ block['incentive'][selected_uid] for block in df ]
+    xx = [x for x, _ in sorted(zip(x, y))]
+    yy = [y for _, y in sorted(zip(x, y))]
+    return px.line( x=xx, y=yy, template='plotly_dark', markers=True, title="Incentive @ uid: {}".format( selected_uid ), labels=dict(x="block", y="incentive") )
 
 if __name__ == '__main__':
-    app.run_server(host = '0.0.0.0', debug=False)
+    app.run_server(host = '0.0.0.0', debug=True)
+
